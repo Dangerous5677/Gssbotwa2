09d2fb2b72d047a22dbef7323859d670065c8e6b
Dangerous5677 <109791541+Dangerous5677@users.noreply.github.com>
2024-07-14 15:44:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://chat.whatsapp.com/BNE0V8XpEZK0q4IgJ9jklM'
+global.linkGroup = 'https://chat.whatsapp.com/BNE0V8XpEZK0q4IgJ9jklM'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.ownername = "*༺ု᪳𝐒ɦ𝛂ͥƞι 𝐑𝛂ᴊͣᴘͫʊʈ⁹⁹͢⁴ှ᪳༻*";
+global.owner = ['923455116994'];
 global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.packname = "'ꜛ-🫂❤‍🩹⏤͟͟͞͞᚜ု᪳ₒ͞₃₄₅ᷤ₅ⷩ₁ⷶ₁ᷡ₆ͥ₉͞₉₄ှ᪳᚛͟͞⏤💍-↑;
+global.author = ".";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
-global.autoswview = true
+global.autoswview = false
 global.autoread = false
 global.available = false
 global.autoTyping = false
 global.autoRecord = false
-global.autoreact = false
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
